30f100c5e9ff1bfdea797d7921f893f4d01fafd7
Mike Cooper <mikecoop@users.noreply.github.com>
2020-01-17 17:03:23 -0500

    Finish lesson 9

---
+++ Lesson9/scratchpad.fsx
@@ -0,0 +1,28 @@
+﻿
+let parseName (name:string) =
+    let parts = name.Split (' ')
+    let forename = parts.[0]
+    let surname = parts.[1]
+    forename, surname
+
+let name = parseName ("Michael Cooper")
+let forename, surname = name
+let fname, sname = parseName ("Michael Cooper")
+
+let parse (person:string) =
+    let parts = person.Split (' ')
+    let playerName = parts.[0]
+    let game = parts.[1]
+    let score = int parts.[2]
+    playerName, game, score
+
+let value = parse "Mary Asteroids 2500"
+let n, g, s = value
+
+open System.IO
+
+let getFileNameAndLastModifiedTime (file:string) =
+    let lastModified = File.GetLastWriteTime file
+    file, lastModified
+
+getFileNameAndLastModifiedTime @"C:\Users\mcooper\Downloads\get-programming-with-fsharp\src\code-listings\lesson-08\Program.cs"
