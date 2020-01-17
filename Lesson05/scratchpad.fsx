f88967d43ea17312e4731e5c144d0fbfe58fda0f
Mike Cooper <mikecoop@users.noreply.github.com>
2020-01-17 14:45:11 -0500

    Finish lesson 6

---
+++ Lesson05/scratchpad.fsx
@@ -0,0 +1,41 @@
+﻿
+let add (a:int, b:int) : int =
+    let answer:int = a + b
+    answer
+
+let add1 (a:int, b:int) =
+    let answer:int = a + b
+    answer
+
+let add2 (a, b) =
+    let answer = a + b
+    answer
+
+open System.Collections.Generic
+let numbers = List<_>()
+numbers.Add(10)
+numbers.Add(20)
+
+let otherNumbers = List()
+otherNumbers.Add(10)
+otherNumbers.Add(20)
+
+let createList (first, second) =
+    let output = List()
+    output.Add first
+    output.Add second
+    output
+
+let sayHello (someValue) =
+    let innerFunction(number) =
+        if number > 10 then "Isaac"
+        elif number > 20 then "Fred"
+        else "Sara"
+
+    let resultOfInner =
+        if someValue < 10.0 then innerFunction(5)
+        else innerFunction(15)
+
+    "Hello " + resultOfInner
+
+let result = sayHello(10.5)
---
+++ Lesson06/scratchpad.fsx
@@ -0,0 +1,32 @@
+﻿
+open System.Windows.Forms
+let form = new Form(Text = "Hello from F#!", Width = 300, Height = 300)
+
+let mutable petrol = 100.0
+
+let drive distance =
+    if distance = "far" then petrol <- petrol / 2.0
+    elif distance = "medium" then petrol <- petrol - 10.0
+    else petrol <- petrol - 1.0
+
+drive "far"
+drive "medium"
+drive "short"
+
+petrol
+
+let drive1 petrol distance =
+    if distance = "far" then petrol / 2.0
+    elif distance = "medium" then petrol - 10.0
+    else petrol - 1.0
+
+let petrol1 = 100.0
+let firstState = drive1 petrol1 "far"
+let secondState = drive1 firstState "medium"
+let thirdState = drive1 secondState "short"
+
+let drive2 petrol distance =
+    if distance > 50 then petrol / 2.0
+    elif distance > 25 then petrol - 10.0
+    elif distance > 1 then petrol - 1.0
+    else petrol
