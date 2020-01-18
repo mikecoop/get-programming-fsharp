9b0b07d23def8f8f420864cee8b9fc3a380853b8
Mike Cooper <mikecoop@users.noreply.github.com>
2020-01-18 15:43:29 -0500

    Finish lesson 17 and 18

---
+++ Lesson17/scratchpad.fsx
@@ -0,0 +1,11 @@
+﻿open System.IO
+open System
+
+let directories = Directory.EnumerateDirectories @"C:\"
+let result =
+    directories
+    |> Seq.map DirectoryInfo
+    |> Seq.map (fun d -> d.Name, d.CreationTimeUtc)
+    |> Map.ofSeq
+    |> Map.map (fun _ time -> (DateTime.UtcNow - time).TotalDays)
+    |> Map.toList
---
+++ Lesson18/scratchpad.fsx
@@ -0,0 +1,34 @@
+﻿
+let sum inputs =
+    let mutable accumulator = 0
+    for input in inputs do
+        accumulator <- accumulator + input
+    accumulator
+
+let length inputs =
+    let mutable accumulator = 0
+    for _ in inputs do
+        accumulator <- accumulator + 1
+    accumulator
+
+let maximum inputs =
+    let mutable accumulator = 0
+    for input in inputs do
+        if input > accumulator then
+            accumulator <- input
+        else ()
+    accumulator
+
+let sum1 inputs =
+    Seq.fold
+        (fun state input -> state + input)
+        0
+        inputs
+
+let length1 = Seq.fold (fun s _ -> s + 1) 0
+
+let max1 = Seq.fold (fun s i -> max s i) 0
+
+[ 1; 2; 3; 4; 5 ] |> length1
+
+[ 6; 1; 2; 3; 9 ] |> max1
