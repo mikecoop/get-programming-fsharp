f360edbeffa86087c9a0946a52743e9c88b1818d
Mike Cooper <mikecoop@users.noreply.github.com>
2020-01-17 23:57:24 -0500

    Finish lesson 12

---
+++ Lesson10/scratchpad.fsx
@@ -0,0 +1,39 @@
+﻿
+type Address =
+    { Street: string
+      Town: string
+      City: string }
+
+type Customer =
+    { Forename: string
+      Surname: string
+      Age: int
+      Address: Address
+      EmailAddress: string }
+
+let customer =
+    { Forename = "Joe"
+      Surname = "Bloggs"
+      Age = 30
+      Address =
+        { Street = "The Street"
+          Town = "The Town"
+          City = "The City" }
+      EmailAddress = "joe@bloggs.com" }
+      
+let updatedCustomer =
+    { customer with
+        Age = 31
+        EmailAddress = "joe@bloggs.co.uk" }
+
+let address1 =
+    { Street = "Street"
+      Town = "Town"
+      City = "City" }
+
+let address2 =
+    { Street = "Street"
+      Town = "Town"
+      City = "City" }
+
+address1 = address2
---
+++ Lesson11/scratchpad.fsx
@@ -0,0 +1,44 @@
+﻿
+let tupledAdd (a, b) = a + b
+
+let answer1 = tupledAdd (1, 2)
+
+let curriedAdd a b = a + b
+
+let answer2 = curriedAdd 1 2
+
+let add first second = first + second
+
+let addFive = add 5
+
+let fifteen = addFive 10
+
+open System
+
+let buildDt year month day = DateTime(year, month, day)
+let buildDtThisYear = buildDt DateTime.UtcNow.Year
+let buildDtThisMonth = buildDtThisYear DateTime.UtcNow.Month
+
+open System.IO
+
+let writeToFile (date:DateTime) filename text =
+    let path = sprintf "%A-%A.txt" (date.ToString "yyMMdd") filename
+    File.WriteAllText (path, text)
+
+let time =
+    let directory = Directory.GetCurrentDirectory()
+    Directory.GetCreationTime directory
+
+let drive distance petrol = petrol
+
+let startingPetrol = 100.0
+let petrol1 = drive startingPetrol "far"
+let petrol2 = drive petrol1 "medium"
+let petrol3 = drive petrol2 "short"
+
+startingPetrol |>
+drive "far" |>
+drive "medium" |>
+drive "short"
+
+
