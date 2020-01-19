76fa10cd492c28f2edeb0ecb0628c4562154207a
Mike Cooper <mikecoop@users.noreply.github.com>
2020-01-19 10:25:02 -0500

    Finish lesson 20 and 21

---
+++ Lesson20/scratchpad.fsx
@@ -0,0 +1,8 @@
+﻿
+let getCreditLimit customer =
+    match customer with
+    | "medium", 1 -> 500
+    | "good", 0 | "good", 1 -> 750
+    | "good", 2 -> 1000
+    | "good", _ -> 2000
+    | _ -> 250
---
+++ Lesson21/scratchpad.fsx
@@ -0,0 +1,21 @@
+﻿
+type Disk =
+| HardDisk of Rpm: int * Platters: int
+| SolidState
+| MMC of NumberOfPins: int
+
+let myHardDisk = HardDisk (Rpm = 2500, Platters = 7)
+let myHardDiskShort = HardDisk (2500, 7)
+let args = 250, 7
+let myHardDiskTupled = HardDisk args
+let myMMC = MMC 5
+let mySSD = SolidState
+
+let describe = function
+    | SolidState -> "I'm a newfangled SSD."
+    | MMC 1 -> "I have only 1 pin."
+    | MMC pins when pins < 5 -> "I'm an MMC with a few pins."
+    | MMC pins -> sprintf "I'm an MMC with %i pins." pins
+    | HardDisk (5400, 0) -> "I'm a slow hard disk."
+    | HardDisk (_, 7) -> "I have 7 spindles!"
+    | HardDisk _ -> "I'm a hard disk."
