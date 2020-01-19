c931b1f9024ca075dcc07debfaae76ad8118af74
Mike Cooper <mikecoop@users.noreply.github.com>
2020-01-19 13:06:52 -0500

    Finish lesson 22 and 23

---
+++ Lesson23/scratchpad.fsx
@@ -0,0 +1,33 @@
+﻿
+type CustomerId = CustomerId of string
+
+type ContactDetails =
+| Address of string
+| Telephone of string
+| Email of string
+
+type Customer =
+    { CustomerId: CustomerId
+      PrimaryContactDetails: ContactDetails
+      SecondaryContactDetails: ContactDetails option }
+
+type GeniuineCustomer = GenuineCustomer of Customer
+
+let createCustomer customerId primary secondary =
+    { CustomerId = customerId
+      PrimaryContactDetails = primary
+      SecondaryContactDetails = secondary }
+
+let validateCustomer customer =
+    match customer.PrimaryContactDetails with
+    | Email e when e.EndsWith "SuperCorp.com" -> Some (GenuineCustomer customer)
+    | Address _ | Telephone _ -> Some (GenuineCustomer customer)
+    | Email _ -> None
+
+let sendWelcomeEmail (GenuineCustomer customer) =
+    printfn "Hello, %A, and welcome to our site!" customer.CustomerId
+
+let customer =
+    createCustomer
+        (CustomerId "C-123")
+        (Email "nicki@myemail.com")
